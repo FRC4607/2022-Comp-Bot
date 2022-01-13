730c39dc0be971adf687aab00207ab9463f7aa21
Tim Middleton <middletontimothy53@gmail.com>
2022-01-12 21:25:30 -0600

    Fixed range correction in getGyroscopeReading and fixed names

--- src/main/java/frc/robot/commands/DrivetrainJoystick.java
+++ src/main/java/frc/robot/commands/DrivetrainJoystick.java
@@ -56,6 +56,9 @@ public class DrivetrainJoystick extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        double xAxis = m_driver.getRawAxis(0);
+        double yAxis = m_driver.getRawAxis(1);
+        m_drivetrainSubsystem.setArcadeDrive(xAxis, yAxis);
     }
 
     // Called once the command ends or is interrupted.
