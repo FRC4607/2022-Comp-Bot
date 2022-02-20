0447f83ea001febf84f1ffab388a42576ca708e5
Tim Middleton <middletontimothy53@gmail.com>
2022-02-19 22:29:10 -0600

    Pathplaner

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -10,10 +10,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.CANCoderConfiguration;
-import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.sensors.Pigeon2;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
-import com.ctre.phoenix.sensors.PigeonIMU.GeneralStatus;
-import com.ctre.phoenix.sensors.PigeonIMU.PigeonState;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
@@ -35,8 +33,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  *
  */
 public class DrivetrainSubsystem extends SubsystemBase {
-    private CANCoder m_leftEncoder;
-    private CANCoder m_rightEncoder;
+    // private CANCoder m_leftEncoder;
+    // private CANCoder m_rightEncoder;
     private WPI_TalonFX m_leftMotor1;
     private WPI_TalonFX m_leftMotor2;
     private WPI_TalonFX m_rightMotor1;
@@ -44,10 +42,9 @@ public class DrivetrainSubsystem extends SubsystemBase {
     private MotorControllerGroup m_leftDrive;
     private MotorControllerGroup m_rightDrive;
     private DifferentialDrive m_drivetrain;
-    private final CANCoderConfiguration m_encoderConfig;
+    // private final CANCoderConfiguration m_encoderConfig;
     private final TalonFXConfiguration m_motorConfig;
-    private PigeonIMU m_pigeonIMU;
-    private boolean m_pigeonFailure = false;
+    private Pigeon2 m_pigeonIMU;
     private final SlewRateLimiter m_rateLimiter;
 
     private final DifferentialDriveOdometry m_odometry;
@@ -65,38 +62,39 @@ public class DrivetrainSubsystem extends SubsystemBase {
     *
     */
     public DrivetrainSubsystem() {
-        m_encoderConfig = new CANCoderConfiguration();
-        m_encoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
-        m_encoderConfig.initializationStrategy = SensorInitializationStrategy.BootToZero;
-        m_encoderConfig.sensorCoefficient = DriveConstants.sensorCoefficient;
-        m_encoderConfig.unitString = "meters";
+        // m_encoderConfig = new CANCoderConfiguration();
+        // m_encoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+        // m_encoderConfig.initializationStrategy = SensorInitializationStrategy.BootToZero;
+        // m_encoderConfig.sensorCoefficient = DriveConstants.sensorCoefficient;
+        // m_encoderConfig.unitString = "meters";
 
-        m_leftEncoder = new CANCoder(DriveConstants.leftEncoderID);
-        m_leftEncoder.configFactoryDefault();
-        m_leftEncoder.configAllSettings(m_encoderConfig);
-        m_leftEncoder.configSensorDirection(true);
+        // m_leftEncoder = new CANCoder(DriveConstants.leftEncoderID);
+        // m_leftEncoder.configFactoryDefault();
+        // m_leftEncoder.configAllSettings(m_encoderConfig);
+        // m_leftEncoder.configSensorDirection(true);
 
-        m_rightEncoder = new CANCoder(DriveConstants.rightEncoderID);
-        m_rightEncoder.configFactoryDefault();
-        m_rightEncoder.configAllSettings(m_encoderConfig);
+        // m_rightEncoder = new CANCoder(DriveConstants.rightEncoderID);
+        // m_rightEncoder.configFactoryDefault();
+        // m_rightEncoder.configAllSettings(m_encoderConfig);
 
         m_motorConfig = new TalonFXConfiguration();
         m_motorConfig.supplyCurrLimit = new SupplyCurrentLimitConfiguration(true, 35, 40, 0.2);
-        //m_motorConfig.statorCurrLimit = new StatorCurrentLimitConfiguration(true, 20, 40, 0.1);
+        // m_motorConfig.statorCurrLimit = new StatorCurrentLimitConfiguration(true, 20,
+        // 40, 0.1);
         m_motorConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
-        m_motorConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.CANCoder;
-        m_motorConfig.remoteFilter0.remoteSensorDeviceID = m_leftEncoder.getDeviceID();
-        m_motorConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.CANCoder;
-        m_motorConfig.remoteFilter1.remoteSensorDeviceID = m_rightEncoder.getDeviceID();
+        // m_motorConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.CANCoder;
+        // m_motorConfig.remoteFilter0.remoteSensorDeviceID = m_leftEncoder.getDeviceID();
+        // m_motorConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.CANCoder;
+        // m_motorConfig.remoteFilter1.remoteSensorDeviceID = m_rightEncoder.getDeviceID();
 
         m_leftMotor1 = new WPI_TalonFX(DriveConstants.leftMotor1ID);
         m_leftMotor1.configFactoryDefault();
         m_leftMotor1.configAllSettings(m_motorConfig);
-        //m_leftMotor1.setNeutralMode(NeutralMode.Brake);
+        // m_leftMotor1.setNeutralMode(NeutralMode.Brake);
         m_leftMotor2 = new WPI_TalonFX(DriveConstants.leftMotor2ID);
         m_leftMotor2.configFactoryDefault();
         m_leftMotor2.configAllSettings(m_motorConfig);
-        //m_leftMotor2.setNeutralMode(NeutralMode.Brake);
+        // m_leftMotor2.setNeutralMode(NeutralMode.Brake);
 
         m_leftDrive = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
         addChild("LeftDrive", m_leftDrive);
@@ -105,12 +103,12 @@ public class DrivetrainSubsystem extends SubsystemBase {
         m_rightMotor1.configFactoryDefault();
         m_rightMotor1.configAllSettings(m_motorConfig);
         m_rightMotor1.setInverted(true);
-        //m_rightMotor1.setNeutralMode(NeutralMode.Brake);
+        // m_rightMotor1.setNeutralMode(NeutralMode.Brake);
         m_rightMotor2 = new WPI_TalonFX(DriveConstants.rightMotor2ID);
         m_rightMotor2.configFactoryDefault();
         m_rightMotor2.configAllSettings(m_motorConfig);
         m_rightMotor2.setInverted(true);
-        //m_rightMotor2.setNeutralMode(NeutralMode.Brake);
+        // m_rightMotor2.setNeutralMode(NeutralMode.Brake);
 
         m_rightDrive = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
         addChild("RightDrive", m_rightDrive);
@@ -124,9 +122,11 @@ public class DrivetrainSubsystem extends SubsystemBase {
         m_drivetrain.setMaxOutput(1.0);
 
         // ADISIMU = new ADIS16470_IMU();
-        m_pigeonIMU = new PigeonIMU(DriveConstants.pigeonID);
+        m_pigeonIMU = new Pigeon2(DriveConstants.pigeonID);
 
-        m_pigeonIMU.setFusedHeading(0);
+        m_pigeonIMU.configFactoryDefault();
+        m_pigeonIMU.configMountPose(-90, -90, 0);
+        m_pigeonIMU.setYaw(0);
 
         m_odometry = new DifferentialDriveOdometry(
                 new Rotation2d(getGyroscopeReadingRad()));
@@ -139,11 +139,15 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        m_odometry.update(getRotation2d(), m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
-        // SmartDashboard.putNumber("Front Left Voltage", m_leftMotor1.getMotorOutputVoltage());
-        // SmartDashboard.putNumber("Back Left Voltage", m_leftMotor2.getMotorOutputVoltage());
-        // SmartDashboard.putNumber("Front Right Voltage", m_rightMotor1.getMotorOutputVoltage());
-        // SmartDashboard.putNumber("Back Right Voltage", m_rightMotor2.getMotorOutputVoltage());
+        // m_odometry.update(getRotation2d(), m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
+        // SmartDashboard.putNumber("Front Left Voltage",
+        // m_leftMotor1.getMotorOutputVoltage());
+        // SmartDashboard.putNumber("Back Left Voltage",
+        // m_leftMotor2.getMotorOutputVoltage());
+        // SmartDashboard.putNumber("Front Right Voltage",
+        // m_rightMotor1.getMotorOutputVoltage());
+        // SmartDashboard.putNumber("Back Right Voltage",
+        // m_rightMotor2.getMotorOutputVoltage());
         // SmartDashboard.putNumber("Left Encoder", m_leftEncoder.getPosition());
         // SmartDashboard.putNumber("Right Encoder", m_rightEncoder.getPosition());
     }
@@ -224,32 +228,12 @@ public class DrivetrainSubsystem extends SubsystemBase {
     // Encoders
 
     /**
-     * Returns the left encoder absolute position.
-     * 
-     * @return The absolute position of the left encoder ranging from [0, 0.47877)
-     *         in meters.
-     */
-    public double getLeftEncoderAbsolutePosition() {
-        return m_leftEncoder.getAbsolutePosition();
-    }
-
-    /**
-     * Returns the right encoder absolute position.
-     * 
-     * @return The absolute position of the right encoder ranging from [0, Math.PI
-     *         Units.inchesToMeters(6)) in meters.
-     */
-    public double getRightEncoderAbsolutePosition() {
-        return m_rightEncoder.getAbsolutePosition();
-    }
-
-    /**
      * Returns the left encoder position.
      * 
      * @return The position of the right side of the drivetrain in meters.
      */
     public double getLeftEncoderPosition() {
-        return m_leftEncoder.getPosition();
+        return 0; // m_leftEncoder.getPosition();
     }
 
     /**
@@ -258,7 +242,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * @return The position of the right side of the drivetrain in meters.
      */
     public double getRightEncoderPosition() {
-        return m_rightEncoder.getPosition();
+        return 0;// m_rightEncoder.getPosition();
     }
 
     /**
@@ -267,7 +251,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * @param position The position of the left side of the drivetrain in meters.
      */
     public void setLeftEncoderPostion(double position) {
-        m_leftEncoder.setPosition(position);
+        // m_leftEncoder.setPosition(position);
     }
 
     /**
@@ -276,7 +260,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * @param position The position of the right side of the drivetrain in meters.
      */
     public void setRightEncoderPostion(double position) {
-        m_rightEncoder.setPosition(position);
+        // m_rightEncoder.setPosition(position);
     }
 
     /**
@@ -285,8 +269,8 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * @param position The position of the two sides of the drivetrain in meters.
      */
     public void setEncoderPostion(double position) {
-        m_leftEncoder.setPosition(position);
-        m_rightEncoder.setPosition(position);
+        // m_leftEncoder.setPosition(position);
+        // m_rightEncoder.setPosition(position);
     }
 
     /**
@@ -300,14 +284,14 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * Return the Velocity of the left Encoder
      */
     public double getLeftVelocity() {
-        return m_leftEncoder.getVelocity();
+        return 0; // m_leftEncoder.getVelocity();
     }
 
     /**
      * Return the Velocity of the right Encoder
      */
     public double getRightVelocity() {
-        return m_rightEncoder.getVelocity();
+        return 0; // m_rightEncoder.getVelocity();
     }
 
     // Gyroscope
@@ -321,23 +305,6 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * measures without converting them first.
      * - Rotation2d goes from -inf to inf, so you can use it continuously.
      */
-
-    /**
-     * Check to see if our pigeon has disconnected. Useful for switching to a backup
-     * IMU if present.
-     * 
-     * @return True if the pigeon has failed, false otherwise.
-     */
-    private boolean checkForPigeonFailure() {
-        GeneralStatus status = new GeneralStatus();
-        m_pigeonIMU.getGeneralStatus(status);
-        if (status.state == PigeonState.NoComm || status.state == PigeonState.Unknown) {
-            m_pigeonFailure = true;
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     /**
      * Returns the robot's heading, ranging from -inf to inf, with counterclockwise
@@ -359,24 +326,12 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * @return The rotation of the robot in degrees.
      */
     public double getGyroscopeReadingContinuous(boolean ccwPositive) {
-        if (!m_pigeonFailure && !checkForPigeonFailure()) {
-            if (ccwPositive) {
-                return m_pigeonIMU.getFusedHeading();
-            } else {
-                return -m_pigeonIMU.getFusedHeading();
-            }
+        if (ccwPositive) {
+            return m_pigeonIMU.getYaw();
         } else {
-            // Can't use these due to Analog Devices gyro bug.
-            /*
-             * double uncorrectedHeading = ADISIMU.getAngle();
-             * if (ccwPositive) {
-             * return uncorrectedHeading;
-             * } else {
-             * return -uncorrectedHeading;
-             * }
-             */
-            return 0;
+            return -m_pigeonIMU.getYaw();
         }
+
     }
 
     /**
@@ -469,7 +424,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
      * @return The current wheel speeds.
      */
     public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-        return new DifferentialDriveWheelSpeeds(m_leftEncoder.getVelocity(), m_rightEncoder.getVelocity());
+        return new DifferentialDriveWheelSpeeds( 0, 0); //m_leftEncoder.getVelocity(), m_rightEncoder.getVelocity());
     }
 
     // PID, FF, Ramset
@@ -487,7 +442,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
         return m_leftController.calculate(getWheelSpeeds().leftMetersPerSecond,
                 leftSpeedSetpoint);
     }
-    
+
     public double getRightPID(double rightSpeedSetpoint) {
         return m_rightController.calculate(getWheelSpeeds().leftMetersPerSecond,
                 rightSpeedSetpoint);
@@ -503,6 +458,6 @@ public class DrivetrainSubsystem extends SubsystemBase {
     }
 
     public void setBrakeMode(boolean brakeMode) {
-        m_rightMotor1.setNeutralMode(brakeMode? NeutralMode.Brake:NeutralMode.Coast);
+        m_rightMotor1.setNeutralMode(brakeMode ? NeutralMode.Brake : NeutralMode.Coast);
     }
 }
